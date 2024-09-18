739c4fec4eb09ec7d4369521b3c32a6b5232fc76
mahesh1310520 <maheshbojja783@gmail.com>
2024-09-18 08:26:29 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,10 @@
+$("#l1").css("backgroundColor","yellow")
+
+$("#l1").parent().css("backgroundColor","lime")
+
+$("#l1").parents().css("border","3px solid purple")
+
+$("#l1").parentsUntil(".box1").css("border","3px solid aqua")
+
+$(".para2").parentsUntil("body").css("border","3px solid orange")
+
